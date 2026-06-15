915f946ea3c38bcde933c9a8d414526d3471f364
U-Gabriel <gabrielarie2000@gmail.com>
2026-06-15 11:45:48 +0200

    Update mail_service.mjs

--- ajpt/services/mail_service.mjs
+++ ajpt/services/mail_service.mjs
@@ -4,7 +4,9 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const transporter = nodemailer.createTransport({
-    service: 'gmail',
+    host: process.env.EMAIL_HOST,
+    port: process.env.EMAIL_PORT,
+    secure: true,
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
