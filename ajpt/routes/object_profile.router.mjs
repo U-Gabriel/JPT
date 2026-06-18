7a90017b9b79419215ae4a01ce9e513d6fc62039
U-Gabriel <gabrielarie2000@gmail.com>
2026-06-19 00:50:08 +0200

    Update object_profile.router.mjs

--- ajpt/routes/object_profile.router.mjs
+++ ajpt/routes/object_profile.router.mjs
@@ -22,14 +22,14 @@ routerObjectProfile.post("/object_profile/detail/byop", async (req, res) => {
 });
 
 routerObjectProfile.post("/object_profile/delete/byobjectprofile", async (req, res) => {
-    const response = await DeleteObjectProfileController(req.body);
+    const response = await DeleteObjectProfileController(req);
     res.status(response.code).send(response);
 });
 
-routerObjectProfile.post("/object_profile/delete/byobjectprofile", async (req, res) => {
+/*routerObjectProfile.delete("/object_profile/delete/byobjectprofile", async (req, res) => {
     const response = await DeleteObjectProfileController(req);
     res.status(response.code).json(response);
-});
+});*/
 
 routerObjectProfile.patch("/object/object_profile/update/byobjectprofile", objectAuth, async (req, res) => {
     const response = await UpdateObjectProfileController(req.body)
