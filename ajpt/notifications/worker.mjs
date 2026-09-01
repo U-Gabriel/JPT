c0c08d112ad0c19105f10270b6f359a8bad23887
U-Gabriel <gabrielarie2000@gmail.com>
2026-09-01 20:24:57 +0200

    Update worker.mjs

--- ajpt/notifications/worker.mjs
+++ ajpt/notifications/worker.mjs
@@ -1,4 +1,4 @@
-import { startNotificationScheduler } from './notifications/notificationScheduler.mjs';
+import { startNotificationScheduler } from './notificationScheduler.mjs';
 
 console.log("[Worker] Démarrage du processus d'arrière-plan pour les tâches planifiées...");
 
